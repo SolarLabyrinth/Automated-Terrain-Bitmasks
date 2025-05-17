d7d76e28956ff9b6a9903fd0f9e77c828b195399
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2025-05-16 21:58:34 -0500

    moar

--- addons/bitmask_automation/template_ui.gd
+++ addons/bitmask_automation/template_ui.gd
@@ -12,10 +12,10 @@ const Utils = preload("res://addons/bitmask_automation/utils.gd")
 	get():
 		return tile_map
 	set(value):
-		#if(tile_map):
-			#tile_map.changed.disconnect(setup_source_list)
+		if(tile_map):
+			tile_map.changed.disconnect(setup_source_list)
 		tile_map = value
-		#tile_map.changed.connect(setup_source_list)
+		tile_map.changed.connect(setup_source_list)
 		setup_source_list()
 		check_apply_btn_disabled()
 
@@ -28,6 +28,20 @@ var template_texture: Texture2D:
 			template_texture_rect.texture = template_texture
 		check_apply_btn_disabled()
 
+var item_to_source_map: Dictionary[int, TileSetAtlasSource] = {}
+var source_to_item_map: Dictionary[TileSetAtlasSource, int] = {}
+
+var selected_source_idx: int = 0:
+	get():
+		return selected_source_idx
+	set(value):
+		selected_source_idx = value
+		var source := item_to_source_map.get(selected_source_idx, null)
+		if source:
+			selected_source = source
+		if source_select:
+			source_select.select(selected_source_idx)
+
 var selected_source: TileSetAtlasSource:
 	get():
 		return selected_source
@@ -37,22 +51,25 @@ var selected_source: TileSetAtlasSource:
 			source_texture_rect.texture = selected_source.texture
 		check_apply_btn_disabled()
 
-var item_to_source_map: Dictionary[int, TileSetAtlasSource] = {}
-
 func setup_source_list():
 	clear_item_list()
 	populate_item_list()
+	selected_source_idx = selected_source_idx
 
 func clear_item_list():
 	if source_select:
 		for item_id in range(source_select.item_count - 1, -1, -1):
 			source_select.remove_item(item_id)
+		item_to_source_map = {}
+		source_to_item_map = {}
+
 func populate_item_list():
 	if source_select and tile_map and tile_map.tile_set:
 		for source in Utils.get_atlas_sources(tile_map.tile_set):
 			var name := source.texture.resource_path.split("/")[-1]
 			var id = source_select.add_item(name, source.texture, true)
-			item_to_source_map[id] = source
+			item_to_source_map.set(id, source)
+			source_to_item_map.set(source, id)
 
 func check_apply_btn_disabled():
 	if apply_button:
@@ -63,12 +80,10 @@ func check_apply_btn_disabled():
 
 func _ready() -> void:
 	tile_map = tile_map
-	source_select.select(0)
-	_on_item_list_item_selected(0)
+	selected_source_idx = 0
 
 func _on_item_list_item_selected(index: int) -> void:
-	var source := item_to_source_map[index]
-	selected_source = source
+	selected_source_idx = index
 
 func _on_apply_button_pressed() -> void:
 	if tile_map and template_texture and selected_source:
@@ -77,7 +92,7 @@ func _on_apply_button_pressed() -> void:
 @onready var file_dialog: FileDialog = $FileDialog
 
 func _on_pick_template_button_pressed() -> void:
-	file_dialog.show()
+	file_dialog.popup()
 
 func _on_file_dialog_file_selected(path: String) -> void:
 	var res = load(path)
